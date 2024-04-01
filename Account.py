199faef36ef54c67dfd45df1f434346a01c312db
Adithya <adithya.r.kothur@gmail.com>
2024-04-01 22:30:31 +0530

    fixed bugs

--- Account.py
+++ Account.py
@@ -12,10 +12,12 @@ from firebase_admin import firestore
 import user as customer
 
 cred = credentials.Certificate("link-preview-gen.json")
-try:
-    firebase_admin.get_app()
-except ValueError as e:
-    firebase_admin.initialize_app(cred)
+# try:
+#     firebase_admin.get_app()
+# except ValueError as e:
+#     firebase_admin.initialize_app(cred)
+firebase_admin.initialize_app(cred)
+
 
 def app():
     st.title("Create An Account OR Login")
