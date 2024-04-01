01e8cf43a75cb3380b8f779dcd35d32f851f658d
Adithya <adithya.r.kothur@gmail.com>
2024-04-01 23:25:01 +0530

    fixed ui

--- Home.py
+++ Home.py
@@ -4,6 +4,19 @@ from io import BytesIO
 import requests
 from firebase_admin import firestore
 
-def app():        
+def app():   
+    
+    st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+    )      
     st.title("Welcome to Home") 
     
