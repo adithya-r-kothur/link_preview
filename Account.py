3c648611f1796571eaef65a8307be849350ada3a
Adithya <adithya.r.kothur@gmail.com>
2024-04-01 22:36:42 +0530

    fixed bugs

--- Account.py
+++ Account.py
@@ -12,11 +12,11 @@ from firebase_admin import firestore
 import user as customer
 
 cred = credentials.Certificate("link-preview-gen.json")
-# try:
-#     firebase_admin.get_app()
-# except ValueError as e:
-#     firebase_admin.initialize_app(cred)
-firebase_admin.initialize_app(cred)
+try:
+    firebase_admin.get_app()
+except ValueError as e:
+    firebase_admin.initialize_app(cred)
+# firebase_admin.initialize_app(cred)
 
 
 def app():
